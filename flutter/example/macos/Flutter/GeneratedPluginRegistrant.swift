203cb4dd9b29fe522c87a956b94e6f1a7720adb2
Frank Vollebregt <fvollebregt@evo-it.nl>
2026-02-17 10:44:12 +0100

    Update generated plugin registrant for MacOS

--- flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_secure_storage_macos
-import path_provider_macos
-import shared_preferences_macos
+import flutter_secure_storage_darwin
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
